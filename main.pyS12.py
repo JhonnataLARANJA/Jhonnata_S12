b85dff69d59e85494a7eab49ee2f2fa36499df5c
Jhonnata de Souza Farias <00001101931966sp@al.educacao.sp.gov.br>
2024-06-05 17:30:26 -0300

    Criei a função converter_CM_para_M()

--- main.pyS12.py
+++ main.pyS12.py
@@ -1,8 +1,15 @@
 class conversor_de_medidas:
     def _init_(self):
         pass
-    def converter_CM_para_M():
-        pass
+
+    def converter_CM_para_M(valor_cm):
+        valor_metros=valor_cm /100
+        return valor_metros
+    
     def converter_M_para_CM():
         pass
+    
+resultado= conversor_de_medidas.converter_CM_para_M(100)
+print('Resultado: ', resultado)
+
     
